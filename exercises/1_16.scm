8f80a94d51cec0c150a34b3655186344579da50d
ormr <seraf.gavrilov@gmail.com>
2022-03-19 21:46:49 +0300

    solved 1_16

--- exercises/1_16.scm
+++ exercises/1_16.scm
@@ -1,26 +1,19 @@
 #lang racket
 
-;(define (square x) (* x x))
-
-;(define (fast-expt b n)
-;  (cond ((= n 0) 1)
-;        ((even? n) (square (fast-expt b (/ n 2))))
-;        (else (* b (fast-expt b (- n 1))))))
-
-;(define (even? n) (= (remainder n 2) 0))
-
-;(fast-expt 4 4)
-
-
-(define (square x) (* x x))
 (define (even? n) (= (remainder n 2) 0))
-
-(define (fast-expt-iter b n a)
-  (cond ((= a 0) a)
-        ((even? n) (fast-expt-iter b n (square ())))
-        (else (fast-expt-iter b n (square (* b a))))
+(define (square x) (* x x))
 
 (define (fast-expt b n)
   (fast-expt-iter b n 1))
 
-;(fast-expt 4 0)
+(define (fast-expt-iter b n a)
+  (cond ((= n 0) a)
+        ((even? n) (fast-expt-iter (square b) (/ n 2) (* b n)))
+        (else (fast-expt-iter b (- n 1) (* b a)))))
+
+(define (fast-mult a b)
+  (fast-mult-iter a b n))
+
+(define (fast-mult a b n)
+  (cond ((= n 0) a)
+        ((even? n))
