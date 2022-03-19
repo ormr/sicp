ecfc1577f91595a270d1507d0099c2709ddd9262
ormr <seraf.gavrilov@gmail.com>
2022-03-19 23:47:27 +0300

    213

--- main.scm
+++ main.scm
@@ -1,17 +1,5 @@
 #lang racket
-
-(define (square x) (* x x))
-
-(define (sqrt x)
-  (define (good-enough? guess prev-guess)
-    (< (abs (- prev-guess guess)) 0.000000001))
-  (define (average x y) (/ (+ x y) 2))
-  (define (improve guess x)
-      (average guess (/ x guess)))
-  (define (sqrt-iter guess prev-guess x)
-      (if (good-enough? guess prev-guess)
-        guess
-        (sqrt-iter (improve guess x) guess x)))
-  (sqrt-iter 1.0 0.9 x))
-
-(sqrt 97442958)
+(define (>= x y) (or (> x y) (= x y))) 
+(define (f n)
+  (cond ((< n 3) n)
+    	((>= n 3) (+ (f (- n 1)) (f (- n 2)) (f (- n 3))))))
