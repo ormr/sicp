8d72288779febddda61d650e9ca6899d1e5e3f18
ormr <seraf.gavrilov@gmail.com>
2022-05-04 21:51:15 +0300

    1_30

--- exercises/1_30.scm
+++ exercises/1_30.scm
@@ -4,7 +4,9 @@
 
 (define (sum term a next b)
   (define (iter a result)
-    (if ()
-      ()
-      (iter () ())))
-  (iter () ()))
+    (if (< a b)
+      result
+      (iter (next a) (+ (term a) result))
+  (iter a 0))))
+
+;(sum)
