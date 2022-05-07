638a86efd9370ff52b03a6f4917698d6d37b4ce4
ormr <seraf.gavrilov@gmail.com>
2022-05-07 20:38:21 +0300

    1_32 final

--- exercises/1_32.scm
+++ exercises/1_32.scm
@@ -1,5 +1,13 @@
 #lang sicp
 
+; Комбинаторы
+; Сумма
+(define (add a b) (+ a b))
+
+; Произведение
+(define (mult a b) (* a b))
+
+; Абстракция накопления
 ; Рекурсивный процесс
 (define (accumulate combiner null-value term a next b)
   (if (> a b)
@@ -13,3 +21,25 @@
         result
         (iter (next a) (combiner (term a) result))))
   (iter a null-value))
+
+; Применение
+
+(define (identity x) x)
+
+(define (inc x) (+ x 1))
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (accumulate add 0 term a next b))
+
+(define (product term a next b)
+  (accumulate-iter mult 1 term a next b))
+
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (product-integers a b)
+  (product identity a inc b))
+
+(sum-integers 1 10)
+(product-integers 1 10)
